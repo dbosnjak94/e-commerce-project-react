7cb7919c647f551c4ff82f101972f89cdef1a040
Dominik Bosnjak <dxb3501@g.rit.edu>
2023-01-16 21:23:23 +0100

    refactoring index page exports

--- src/App.js
+++ src/App.js
@@ -2,8 +2,22 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Navbar, Sidebar, Footer } from "./components";
 
+import {
+  Home,
+  SingleProduct,
+  Cart,
+  Error,
+  Checkout,
+  About,
+  PrivateRoute,
+} from "./pages/index";
+
 function App() {
-  return <h4>comfy sloth starter</h4>;
+  return (
+    <div className="">
+      <Home />
+    </div>
+  );
 }
 
 export default App;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,21 @@
-import AboutPage from "./AboutPage";
+import About from "./AboutPage";
 import AuthWrapper from "./AuthWrapper";
-import CartPage from "./CartPage";
-import CheckoutPage from "./CheckoutPage";
-import ErrorPage from "./ErrorPage";
-import HomePage from "./HomePage";
+import Cart from "./CartPage";
+import Checkout from "./CheckoutPage";
+import Error from "./ErrorPage";
+import Home from "./HomePage";
 import PrivateRoute from "./PrivateRoute";
-import SingleProductPage from "./SingleProductPage";
-import ProductsPage from "./ProductsPage";
+import SingleProduct from "./SingleProductPage";
+import Products from "./ProductsPage";
 
 export {
-  AboutPage,
+  About,
   AuthWrapper,
-  CartPage,
-  CheckoutPage,
-  ErrorPage,
-  HomePage,
+  Cart,
+  Checkout,
+  Error,
+  Home,
   PrivateRoute,
-  SingleProductPage,
-  ProductsPage,
+  SingleProduct,
+  Products,
 };
