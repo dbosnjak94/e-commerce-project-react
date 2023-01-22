d98e14518d99c371aa5840e5b0587b6c44d99d64
Dominik Bosnjak <dxb3501@g.rit.edu>
2023-01-22 20:59:11 +0100

    Sidebar - hide/show logic REFACTOR

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -13,7 +13,7 @@ const Sidebar = () => {
 
   return (
     <SidebarContainer>
-      <aside className={`${isOpen ? "sidebar show-sidebar" : "show-sidebar"}`}>
+      <aside className={`${isOpen ? "sidebar show-sidebar" : "sidebar"}`}>
         <div className="sidebar-header">
           <img src={logo} className="logo" alt="comfy sloth" />
           <button className="close-btn" type="button">
