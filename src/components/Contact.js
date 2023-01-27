b599211de847fe4055137f1ce88e3cf219e8e6e2
Dominik Bosnjak <dxb3501@g.rit.edu>
2023-01-27 10:39:05 +0100

    Contact Component - styling services

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,35 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
 const Contact = () => {
-  return <h4>contact section</h4>
-}
+  return (
+    <Wrapper>
+      <div className="section-center">
+        <h3>Join our newsletter and get 20% off</h3>
+        <div className="content">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum modi
+            consequatur fugit rem tempore, omnis perspiciatis et doloremque a
+            deserunt nesciunt impedit magnam suscipit eius eum, debitis esse
+            quia quod! Nesciunt sit asperiores consequatur quae expedita
+            corporis quidem dolorum perspiciatis quia. Mollitia facilis
+            praesentium sed nihil sunt eum architecto alias.
+          </p>
+          <form className="contact-form">
+            <input
+              type="email"
+              className="form-input"
+              placeholder="enter email"
+            />
+            <button type="submit" className="submit-btn">
+              subscribe
+            </button>
+          </form>
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
 const Wrapper = styled.section`
   padding: 5rem 0;
   h3 {
@@ -67,6 +93,6 @@ const Wrapper = styled.section`
   @media (min-width: 1280px) {
     padding: 15rem 0;
   }
-`
+`;
 
-export default Contact
+export default Contact;
