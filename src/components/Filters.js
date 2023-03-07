90c1ca1ec7b6bdcdb0e620bc488bd6aa7eab0a94
dbosnjak94 <dominik.bosnjak94@gmail.com>
2023-03-07 20:00:35 +0100

    Filters - Setting up Colors

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -80,13 +80,51 @@ const Filters = () => {
               {companies.map((c, index) => {
                 return (
                   <option key={index} value={c}>
-                    {c.toUpperCase()}
+                    {c}
                   </option>
                 );
               })}
             </select>
           </div>
           {/* end of companies */}
+
+          {/* colors */}
+          <div className='form-control'>
+            <h5>colors</h5>
+            <div className='colors'>
+              {colors.map((c, index) => {
+                if (c === 'all') {
+                  return (
+                    <button
+                      name='color'
+                      onClick={updateFilters}
+                      data-color='all'
+                      className={`${
+                        color === 'all' ? 'all-btn active' : 'all-btn'
+                      }`}
+                    >
+                      all
+                    </button>
+                  );
+                }
+                return (
+                  <button
+                    key={index}
+                    name='color'
+                    style={{ background: c }}
+                    className={`${
+                      color === c ? 'color-btn active' : 'color-btn'
+                    }`}
+                    data-color={c}
+                    onClick={updateFilters}
+                  >
+                    {color === c ? <FaCheck></FaCheck> : null}
+                  </button>
+                );
+              })}
+            </div>
+          </div>
+          {/* end of colors */}
         </form>
       </div>
     </Wrapper>
