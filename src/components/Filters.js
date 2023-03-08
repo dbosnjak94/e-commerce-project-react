5d35ba7250eaf77f654cbc9a541b301e44665ccf
dbosnjak94 <dominik.bosnjak94@gmail.com>
2023-03-08 13:14:53 +0100

    Filters - setting up shipping and clear filters

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -140,7 +140,22 @@ const Filters = () => {
             />
           </div>
           {/* end of price */}
+          {/* shipping */}
+          <div className='form-control shipping'>
+            <label htmlFor='shipping'> free shipping</label>
+            <input
+              type='checkbox'
+              name='shipping'
+              id='shipping'
+              onChange={updateFilters}
+              checked={shipping}
+            />
+          </div>
+          {/* end of shipping */}
         </form>
+        <button type='button' className='clear-btn' onClick={clearFilters}>
+          {''} Clear Filters
+        </button>
       </div>
     </Wrapper>
   );
